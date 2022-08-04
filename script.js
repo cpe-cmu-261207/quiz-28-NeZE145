9ae1f9879d05035fc2b6dacd8b05c4e9828a351f
NeZE <boss-NeZE@hotmail.com>
2022-08-04 13:57:54 +0700

    12

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
 }
+
+const btnrandom = document.querySelector("#btn-random");
+const img = document.querySelector("#img-img-profile");
+const name  = document.getElementById()
+
+btnrandom.onclick = async() =>{
+  const resp = await axios.get("https://randomuser.me/api/");
+  img.src = resp.data.
+}
